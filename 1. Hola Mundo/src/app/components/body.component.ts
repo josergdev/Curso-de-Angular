aefd915128df19c979a017d6f00638cc2e876abf
josergfc <joserodriguezgarcia95@gmail.com>
2018-06-20 14:15:35 +0200

    Hola mundo

---
+++ 1. Hola Mundo/src/app/components/body.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html'
+
+})
+export class BodyComponent {
+
+  mostrar = false;
+
+  frase: any = {
+    mensaje: 'Un gran poder conlleva una gran responsabilidad.',
+    autor: 'Ben Parker'
+  };
+
+  personajes: string[] = ['SpiderMan', 'Venom', 'Dr. Octopus'];
+
+}
